5b19af7a9ed6b6d114aff79a4d9c440d52eb12fa
MissBidule <beyondtwome@gmail.com>
2025-12-17 10:59:55 +0100

    Pictures op

---
+++ Assets/Script/CameraCapture.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using UnityEngine;
+
+public class CameraCapture : MonoBehaviour
+{
+    [SerializeField]
+    public int fileCounter;
+    public KeyCode screenshotKey;
+    public Camera Camera;
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(screenshotKey))
+        {
+            Capture();
+        }
+    }
+
+    public void Capture()
+    {
+        RenderTexture activeRenderTexture = RenderTexture.active;
+        RenderTexture.active = Camera.targetTexture;
+
+        Camera.Render();
+
+        Texture2D image = new Texture2D(Camera.targetTexture.width, Camera.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, Camera.targetTexture.width, Camera.targetTexture.height), 0, 0);
+        image.Apply();
+        RenderTexture.active = activeRenderTexture;
+
+        byte[] bytes = image.EncodeToPNG();
+        Destroy(image);
+
+        Directory.CreateDirectory(Application.dataPath + "/Photos/");
+
+        File.WriteAllBytes(Application.dataPath + "/Photos/" + fileCounter++ + ".png", bytes);
+    }
+}
